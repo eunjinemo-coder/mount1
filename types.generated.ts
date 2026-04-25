eea71efc73392208080d4f1f359c4ca68ad7585d
박광준 <homecance@naver.com>
2026-04-25 18:27:43 +0900

    chore: remove stray root types.generated.ts (cmd redirect 부산물)

--- types.generated.ts
+++
@@ -1,7 +0,0 @@
-
-> @mount/db@0.0.0 db:types D:\MOUNT1\packages\db
-> echo 'TODO: supabase gen types typescript --project-id <PROJ_ID> > src/types.generated.ts' "#" "supabase" "gen" "types" "typescript" "--linked"
-
-D:\MOUNT1\packages\db:
- ERR_PNPM_RECURSIVE_RUN_FIRST_FAIL  @mount/db@0.0.0 db:types: `echo 'TODO: supabase gen types typescript --project-id <PROJ_ID> > src/types.generated.ts' "#" "supabase" "gen" "types" "typescript" "--linked"`
-Exit status 1
